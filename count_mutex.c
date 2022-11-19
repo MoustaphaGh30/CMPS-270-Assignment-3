c9ddf65ef873c691cac9164fd8a701d61656c69c
MoustaphaGh30 <112877084+MoustaphaGh30@users.noreply.github.com>
2022-11-19 22:34:10 +0200

    count_mutex

---
+++ count_mutex.c
@@ -0,0 +1,92 @@
+#include <pthread.h>
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+long *array;
+long length = 100000000;
+long count = 0;
+long Number_Of_Threads;
+pthread_mutex_t mutex;
+
+void *count1s(void *thread_ID)
+{
+    long i;
+    long ID = (long)thread_ID;
+    long sub_array_per_thread = length / Number_Of_Threads;
+    long start_index = ID * sub_array_per_thread;
+    long end_index = start_index + sub_array_per_thread;
+    long private_sum=0;
+
+    // some entries may be left out because of integer division
+    // so we assign them to the last thread.
+    if (Number_Of_Threads - ID == 1)
+    {
+        end_index = length;
+    }
+    pthread_mutex_lock(&mutex);
+    for (i = start_index; i < end_index; i++)
+    {
+        if (array[i] == 1)
+        {
+            count++;
+        }
+    }
+    pthread_mutex_unlock(&mutex);
+}
+int main()
+{
+    Number_Of_Threads = 12;
+
+    int CorrectCount = 0;
+    clock_t start, end;
+    double total_time;
+
+    array = (long *)malloc(length * sizeof(long));
+
+
+    start = clock();
+    for (int i = 0; i < length; i++)
+    {
+        array[i] = rand() % 5 + 1;
+    }
+    for (int i = 0; i < length; i++)
+    {
+        if (array[i] == 1)
+            CorrectCount++;
+    }
+    end = clock();
+    total_time = ((double)(end - start)) / CLOCKS_PER_SEC;
+
+
+    printf("The Squential program took %.3f seconds\n", total_time);
+    printf("The Real count is= %ld\n", CorrectCount);
+
+
+    pthread_t Thread[Number_Of_Threads];
+    pthread_mutex_init(&mutex,NULL);
+
+    start = clock();
+
+    for (int i = 0; i < Number_Of_Threads; i++)
+    {
+
+        pthread_create(&Thread[i], NULL, &count1s, (void *)i);
+    }
+
+    for (int i = 0; i < Number_Of_Threads; i++)
+    {
+
+        pthread_join(Thread[i], NULL);
+    }
+
+    end = clock();
+
+    total_time = ((double)(end - start)) / CLOCKS_PER_SEC;
+
+    printf("Took %f seconds with %d threads\n", total_time, Number_Of_Threads);
+    printf("Count after excuting %ld Threads is= %ld\n", Number_Of_Threads, count);
+
+    free(array);
+    return 0;
+}
